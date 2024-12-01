3e3e68f30a5af0a73b5257bcd78d898b8a93b170
aboubakar-salihou <aboubakar.salihou@londo-tech.com>
2024-12-01 16:36:33 +0100

    adding handleError function on commonservice

--- src/app/shared/components/input-email/input-email.component.ts
+++ src/app/shared/components/input-email/input-email.component.ts
@@ -19,22 +19,7 @@ export class InputEmailComponent {
 
   // Regex pour valider l'email
   emailPattern: string = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
-
   // Méthode appelée lors de la saisie
-  onInputChange(event: any): void {
-    const currentValue = this.email;
-    // Validation du format de l'email
-    if (this.isValidEmail(currentValue)) {
-      // Si l'email est valide, émettre l'événement de changement
-      this.emailChange.emit(currentValue);
-    }else {
-      console.log("bad email");
-    }
-  }
+  onInputChange(): void { this.emailChange.emit(this.email); }
 
-  // Fonction de validation d'email
-  isValidEmail(email: string): boolean {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
-  }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -16,9 +16,7 @@ export class AuthService {
   // *** INSCRIPTION UTILISATEUR ***
   async register(email: string, password: string, confirmPassword: string, displayName: string): Promise<void> {
     try {
-      if (password !== confirmPassword) {
-        throw new Error("Les mots de passe ne correspondent pas.");
-      }
+      if (password !== confirmPassword) { throw "PasswordDontMatch"; }
       // Crée l'utilisateur dans Firebase Authentication
       const userCredential: UserCredential = await createUserWithEmailAndPassword(this.auth, email, password);
 
@@ -35,12 +33,7 @@ export class AuthService {
         hashedPassword: hashedPassword,
         createdAt: new Date().toISOString()
       });
-
-      console.log('Utilisateur enregistré avec succès');
-    } catch (error) {
-      console.error('Erreur lors de l\'inscription :', error);
-      throw error;
-    }
+    } catch (error) { throw error; }
   }
 
   // *** CONNEXION UTILISATEUR ***
@@ -54,32 +47,19 @@ export class AuthService {
       const userRef = doc(this.firestore, `${this.usersCollection}/${userId}`);
       const userSnapshot = await getDoc(userRef);
 
-      if (!userSnapshot.exists()) {
-        throw new Error('Utilisateur non trouvé dans la base de données.');
-      }
-
+      if (!userSnapshot.exists()) { throw 'UserNotFound'; }
       const userData = userSnapshot.data();
       const hashedPassword = userData?.['hashedPassword'];
-
       // Vérifie si le mot de passe fourni correspond au mot de passe haché
-      if (!bcrypt.compareSync(password, hashedPassword)) {
-        throw new Error('Mot de passe incorrect.');
-      }
-
-      console.log('Connexion réussie');
-    } catch (error) {
-      console.error('Erreur lors de la connexion :', error);
-      throw error;
-    }
+      if (!bcrypt.compareSync(password, hashedPassword)) { throw 'IncorrectPassword'; }
+    } catch (error) { throw error; }
   }
 
   // *** DÉCONNEXION UTILISATEUR ***
   async logout(): Promise<void> {
     try {
       await signOut(this.auth);
-      console.log('Déconnexion réussie');
     } catch (error) {
-      console.error('Erreur lors de la déconnexion :', error);
       throw error;
     }
   }
